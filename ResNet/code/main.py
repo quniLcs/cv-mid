f1dab9805fbcce52d2c67968993c4d44f817757b
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-04-19 13:11:48 +0800

    Add files via upload

--- ResNet/code/main.py
+++ ResNet/code/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 from torch import nn
 from torchvision.models import resnet18
@@ -7,31 +9,37 @@ from load import load
 from util import initialize, optimize, evaluate, save_status
 
 if __name__ == "__main__":
-    num_classes = 100
-    num_epoch = 30
-    batch_size = 256
+    # python main.py --mode baseline
+    # python main.py --mode cutout
+    # python main.py --mode mixup
+    # python main.py --mode cutmix
 
-    train_loader = load(train = True, batch_size = batch_size, shuffle = True)
-    test_loader = load(train = False, batch_size = batch_size, shuffle = False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_epoch", default = 30, type = int)
+    parser.add_argument("--batch_size", default = 128, type = int)
+    parser.add_argument("--lr", default = 0.1, type = float)
+    parser.add_argument("--momentum", default = 0.9, type = float)
+    parser.add_argument("--lambd", default = 0.005, type = float)
+    parser.add_argument("--mode", default = 'baseline', type = str)
+    args = parser.parse_args()
 
-    print('number of iterations:', num_epoch * len(train_loader))
+    train_loader = load(train = True, batch_size = args.batch_size, shuffle = True)
+    test_loader = load(train = False, batch_size = args.batch_size, shuffle = False)
+
+    print('number of iterations:', args.num_epoch * len(train_loader))
     print('number of iterations per epoch:', len(train_loader))
     print()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = resnet18(num_classes = num_classes).to(device)
+    model = resnet18(num_classes = 100).to(device)
     initialize(model)
 
-    lr = 0.1
-    momentum = 0.9
-    lambd = 0.006
-
-    optimizer = torch.optim.SGD(model.parameters(), lr = lr, momentum = momentum, weight_decay = lambd)
+    optimizer = torch.optim.SGD(model.parameters(), lr = args.lr, momentum = args.momentum, weight_decay = args.lambd)
     criterion = nn.CrossEntropyLoss()
-    writer = SummaryWriter('../runs/baseline')
+    writer = SummaryWriter(args.mode)
 
-    for ind_epoch in range(num_epoch):
-        optimize(model, criterion, optimizer, train_loader, lr, ind_epoch, device)
+    for ind_epoch in range(args.num_epoch):
+        optimize(model, criterion, optimizer, train_loader, args.lr, ind_epoch, args.mode, device)
 
         train_acc, train_loss = evaluate(model, criterion, train_loader, device)
         test_acc, test_loss = evaluate(model, criterion, test_loader, device)
@@ -41,7 +49,7 @@ if __name__ == "__main__":
 
         print('Epoch: %d\tTraining accuracy: %.5f\tTesting Accuracy: %.5f' % (ind_epoch + 1, train_acc, test_acc))
 
-    save_status(model, optimizer, '../result/baseline.pth')
+    save_status(model, optimizer, args.mode + '.pth')
 
     writer.flush()
     writer.close()
--- ResNet/code/util.py
+++ ResNet/code/util.py
@@ -1,6 +1,10 @@
 import torch
 from torch import nn
 
+from cutout import cutout
+from mixup import mixup
+from cutmix import cutmix
+
 
 def initialize(model):
     for module in model.modules():
@@ -14,23 +18,35 @@ def initialize(model):
             nn.init.zeros_(module.bias)
 
 
-def descent_lr(lr, optimizer, ind_epoch):
+def descent_lr(lr, ind_epoch, optimizer):
     lr = lr * 2 ** (-ind_epoch / 5)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
 
-def optimize(model, criterion, optimizer, train_loader, lr, ind_epoch, device):
+def optimize(model, criterion, optimizer, train_loader, lr, ind_epoch, mode, device):
     model.train()
+    descent_lr(lr, ind_epoch, optimizer)
 
-    descent_lr(lr, optimizer, ind_epoch)
+    for inputs, targets in train_loader:
+        inputs = inputs.to(device)
+        targets = targets.to(device)
 
-    for data in train_loader:
-        inputs = data[0].to(device)
-        targets = data[1].to(device)
-
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
+        if mode == 'baseline':
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+        elif mode == 'cutout':
+            cutout(inputs)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+        elif mode == 'mixup':
+            inputs_mixup, targets_mixup, lambd, index = mixup(inputs, targets)
+            outputs = model(inputs_mixup)
+            loss = lambd * criterion(outputs, targets) + (1 - lambd) * criterion(outputs, targets_mixup)
+        else:  # mode == 'cutmix'
+            inputs_cutmix, targets_cutmix, lambd, index = cutmix(inputs, targets)
+            outputs = model(inputs_cutmix)
+            loss = lambd * criterion(outputs, targets) + (1 - lambd) * criterion(outputs, targets_cutmix)
 
         optimizer.zero_grad()
         loss.backward()
@@ -44,9 +60,9 @@ def evaluate(model, criterion, data_loader, device):
     losses = 0
     correct = 0
 
-    for data in data_loader:
-        inputs = data[0].to(device)
-        targets = data[1].to(device)
+    for inputs, targets in data_loader:
+        inputs = inputs.to(device)
+        targets = targets.to(device)
 
         outputs = model(inputs)
         loss = criterion(outputs, targets)
